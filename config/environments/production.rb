7476d143907c9701c3c33e4c7081d7d2208860e8
agdavid <antonio.g.david@gmail.com>
2016-09-26 09:34:35 -0400

    edit production.rb file to relocate s3_region key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,11 @@ Rails.application.configure do
   # Amazon Web Services S3 storage of Paperclip images
   config.paperclip_defaults = {
   storage: :s3,
+  s3_region: ENV['AWS_S3_REGION'],
   s3_credentials: {
     bucket: ENV['AWS_S3_BUCKET'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    s3_region: ENV['AWS_S3_REGION']
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
