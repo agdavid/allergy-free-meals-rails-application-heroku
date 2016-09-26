e8d615d62441f2445e44dd8e80fa51b0163d92f0
agdavid <antonio.g.david@gmail.com>
2016-09-26 15:57:03 -0400

    revert to jQuery/JS instead of Coffeescript in masonry file

---
+++ app/assets/javascripts/masonry.js
@@ -0,0 +1,49 @@
+$(function() {
+
+    $('#recipes').imagesLoaded(function() {
+      $('#recipes').masonry({
+        itemSelector: '.box',
+        isFitWidth: true,
+        });
+      });
+
+    $('#favorited-recipes').imagesLoaded(function() {
+        $('#favorited-recipes').masonry({
+          itemSelector: '.box',
+          isFitWidth: true,
+          });
+        });
+
+    $('#users').imagesLoaded(function() {
+        $('#users').masonry({
+          itemSelector: '.box',
+          isFitWidth: true,
+          });
+        });
+
+    $('#page-refresh').click(function() {
+      var url = "https://allergyfreemeals.herokuapp.com/";
+      window.location.href = url;
+    });
+    
+});
+
+
+// $ ->
+//   $('#recipes').imagesLoaded ->
+//     $('#recipes').masonry({
+//       itemSelector: '.box',
+//       isFitWidth: true
+//     });
+// $ ->
+//   $('#favorited-recipes').imagesLoaded ->
+//     $('#favorited-recipes').masonry({
+//       itemSelector: '.box',
+//       isFitWidth: true
+//     });
+// $ ->
+//   $('#users').imagesLoaded ->
+//     $('#users').masonry({
+//       itemSelector: '.box',
+//       isFitWidth: true
+//     });
