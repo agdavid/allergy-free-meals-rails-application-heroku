2be2c0efde4cc649596f958a12864b87a8d610e6
agdavid <antonio.g.david@gmail.com>
2016-09-26 14:01:07 -0400

    rename to masonry.js

---
+++ app/assets/javascripts/masonry.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('#recipes').imagesLoaded(function() {
+    $('#recipes').masonry({
+      itemSelector: '.box'
+      isFitWidth: true
+      });
+    });
+
+  $('#favorited-recipes').imagesLoaded(function() {
+      $('#favorited-recipes').masonry({
+        itemSelector: '.box'
+        isFitWidth: true
+        });
+      });
+
+  $('#users').imagesLoaded(function() {
+      $('#users').masonry({
+        itemSelector: '.box'
+        isFitWidth: true
+        });
+      });
+});
+  
